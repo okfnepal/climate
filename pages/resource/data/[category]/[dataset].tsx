c88c808fda75717295966aecaf94a995d1f517ad
Zaymes <zaymesshrestha@gmail.com>
2022-08-25 12:52:35 +0545

    path correction for ssg pages

--- pages/resource/data/[category]/[dataset].tsx
+++ pages/resource/data/[category]/[dataset].tsx
@@ -28,7 +28,7 @@ export async function getStaticPaths() {
     const staticPaths: any = []
     response.map((item: any) => {
       item.data.map((items: any) => {
-        staticPaths.push({ params: { dataset: `${items.name.split(' ').join('%20')}`, category: items.path.split('/')[1] } })
+        staticPaths.push({ params: { dataset: `${items.name}`, category: items.path.split('/')[1] } })
       })
     })
     return staticPaths
@@ -41,7 +41,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }: any) {
-  const res = await octokit.request(`GET /repos/okfnepal/climatedata/contents/Datasets/${params.category}/${params.dataset.split(' ').join('%20')}?ref=master`)
+  const res = await octokit.request(`GET /repos/okfnepal/climatedata/contents/Datasets/${params.category}/${params.dataset}?ref=master`)
 
   return {
     props: {
@@ -92,7 +92,7 @@ const Res: NextPage = (props: any) => {
             activeItemStyle={{ color: '#282a35', textDecoration: 'underline' }}
             transformLabel={(label: any) => {
               if (router.asPath.split('/').slice(-1).some(item => item === label)) {
-                return label.split("%20").join(' ')
+                return label.split("-").join(' ')
               } else {
                 return label + ' >'
               }
