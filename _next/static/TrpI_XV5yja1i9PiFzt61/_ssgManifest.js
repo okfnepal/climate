57c84e11b1d7215121d0601a48584efb2b639c05
Zaymes <zaymesshrestha@gmail.com>
2022-09-02 09:23:44 +0000

    Deploying to gh-pages from @ okfnepal/climate@66eec0158fd5ccca2a425eeaa1a347ddd0dfc318 🚀

--- _next/static/TrpI_XV5yja1i9PiFzt61/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fresource\u002Fdata","\u002Fresource\u002Fdata\u002F[category]\u002F[dataset]","\u002Fresource\u002Fjournals","\u002Fresource\u002Fjournals\u002F[category]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/pq5ciKHB2MeV7iTPBT-4N/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fresource\u002Fdata","\u002Fresource\u002Fdata\u002F[category]","\u002Fresource\u002Fdata\u002F[category]\u002F[dataset]","\u002Fresource\u002Fjournals","\u002Fresource\u002Fjournals\u002F[category]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
